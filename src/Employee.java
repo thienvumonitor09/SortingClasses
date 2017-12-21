9c44d8a1532e38d2b5e2713eeecd93f4d1fe1234
Vu Nguyen <thienvumonitor09@gmail.com>
2017-12-21 02:02:58 -0800

    Add javadoc and print list method

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,18 @@
-
-public class Employee implements Comparable<Employee>{
+public class Employee implements Comparable<Employee>
+{
 	private String lastName; 
 	private String firstName; 
 	private String department;
 	
-	public Employee(String lastName, String firstName, String department) {
+	public Employee(String lastName, String firstName, String department) 
+	{
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.department = department;
 	}
 
-	public Employee(String lastName, String firstName) {
+	public Employee(String lastName, String firstName) 
+	{
 		this.lastName = lastName;
 		this.firstName = firstName;
 	}
@@ -19,7 +21,8 @@ public class Employee implements Comparable<Employee>{
 		return lastName;
 	}
 
-	public void setLastName(String lastName) {
+	public void setLastName(String lastName) 
+	{
 		this.lastName = lastName;
 	}
 
@@ -27,24 +30,30 @@ public class Employee implements Comparable<Employee>{
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	public void setFirstName(String firstName) 
+	{
 		this.firstName = firstName;
 	}
 
-	public String getDepartment() {
+	public String getDepartment() 
+	{
 		return department;
 	}
 
-	public void setDepartment(String department) {
+	public void setDepartment(String department) 
+	{
 		this.department = department;
 	}
-
+	
+	/*
+	 * It will sort employee by last name, the first name and then deparment. 
+	 * department can be null. 
+	 */
 	@Override
 	public int compareTo(Employee anotherEmployee) 
 	{
 		if(anotherEmployee == null)
 			throw new NullPointerException("Employee passed  is null");
-		
 		if(this.lastName.equals(anotherEmployee.getLastName()))	
 		{
 			if(this.firstName.equals(anotherEmployee.getFirstName()))
@@ -57,24 +66,21 @@ public class Employee implements Comparable<Employee>{
 					return -1;
 			}
 			else
-			{
 				return this.firstName.compareTo(anotherEmployee.getFirstName());
-			}
 		}
 		else
-		{
 			return this.lastName.compareTo(anotherEmployee.getLastName());
-		}
-		
-			
 	} 
 	
+	/*
+	 * The method is used to print Employee in format
+	 */
+	@Override
 	public String toString()
 	{
-		//StringBuilder sb = new StringBuilder();  
-		//sb.append(lastName);//now original string is changed  
-		//System.out.println(sb);//prints Hello Java  
-		return this.lastName +" " + this.firstName +" " + this.department +"\n";
+		return String.format("%-20s %-20s %10s", this.lastName, this.firstName, this.department).toString();
 	}
+	
+	
 
 }
--- src/SortingClasses.java
+++ src/SortingClasses.java
@@ -1,29 +1,45 @@
 import java.util.*;
-
+/**
+ * The program creates instances of Employee Class 
+ * and sorts List<Employee> by last name, then first name and then deparment.
+ * It can print list by ascending and descending.
+ * 
+ * @author Vu Nguyen
+ *
+ */
 public class SortingClasses {
 
 	public static void main(String[] args) 
 	{
-		//List<Employee> employeeList = null;
+		//Create employeeList and add instances
 		List<Employee> employeeList = new ArrayList<Employee>();
+		employeeList.add(new Employee("Smith","A","Finance"));
+		employeeList.add(new Employee("Johnson","B","Operation"));
+		employeeList.add(new Employee("Williams","C","R&D"));
+		employeeList.add(new Employee("Jones","D","HR"));
+		employeeList.add(new Employee("Brown","E"));
+		employeeList.add(new Employee("Davis","F","HR"));
+		employeeList.add(new Employee("Davis","F"));
+		employeeList.add(new Employee("Wilson","G","Marketing"));
 		
-		employeeList.add(new Employee("Nguyen","Vu","op"));
-		employeeList.add(new Employee("A","Vu","op"));
-		employeeList.add(new Employee("A","Bu","op"));
-		employeeList.add(new Employee("A","Bu","kp"));
-		employeeList.add(new Employee("A","Bu"));
-		employeeList.add(new Employee("A","Vu"));
-		employeeList.add(new Employee("D","Vu","op"));
-		
+		//Print employee list in ascending order.
 		System.out.println("List is sorted in ascending order:");
 		Collections.sort(employeeList);
-		System.out.println(employeeList);
+		printEmployeeList(employeeList);
 		
+		//Print employee list in descending order.
 		System.out.println("List is sorted in descending order:");
 		Collections.reverse(employeeList);
-		System.out.print(employeeList);
-		
-
+		printEmployeeList(employeeList);
 	}
-
+	
+	private static void printEmployeeList(List<Employee> employeeList)
+	{
+		System.out.printf("%-20s %-20s %10s %n", "Last Name", "First Name", "Deparment");	
+		for(Employee e : employeeList) 
+		{
+			System.out.println(e.toString());
+		}
+		System.out.println();
+	}
 }
